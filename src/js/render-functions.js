e76e5b861155c78e0ead596ab9ed7fb104244940
besit06 <behemoth2054@gmail.com>
2024-09-19 16:19:47 +0200

    js11

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -20,7 +20,7 @@ export function renderImages(images) {
     
     const markup = images.map(image => {
     return `
-       <div class="gallery-item">
+      <li class="gallery-item">
         <a href="${image.largeImageURL}" target="_blank">
           <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
         </a>
@@ -29,7 +29,8 @@ export function renderImages(images) {
           <p><b>Views:</b> ${image.views}</p>
           <p><b>Comments:</b> ${image.comments}</p>
           <p><b>Downloads:</b> ${image.downloads}</p>
-      </div>
+        </div>
+      </li>
     `;
   }).join('');
 
