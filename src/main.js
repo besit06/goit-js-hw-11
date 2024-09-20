af04c98f005c43df79116bcfebd15f6cb50a63ce
besit06 <behemoth2054@gmail.com>
2024-09-20 06:55:21 +0200

    js11

--- src/main.js
+++ src/main.js
@@ -4,8 +4,18 @@ import { renderImages, showErrorMessage } from './js/render-functions.js';
 const form = document.querySelector('.search-form');
 const input = form.querySelector('input[name="searchQuery"]');
 const gallery = document.querySelector('.gallery');
+const loader = document.getElementById('loader'); 
 let currentPage = 1;
 const perPage = 20;
+
+
+function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+function hideLoader() {
+  loader.classList.add('hidden');
+}
 
 form.addEventListener('submit', onSearch);
 
@@ -21,16 +31,22 @@ function onSearch(event) {
     return;
   }
 
+  showLoader();
+
   fetchImages(query, currentPage, perPage)
     .then(data => {
+      hideLoader(); 
+
       if (data.hits.length === 0) {
         showErrorMessage('Sorry, there are no images matching your search query. Please try again!');
         return;
       }
-     
+
       renderImages(data.hits);
     })
     .catch(error => {
+      hideLoader(); 
       showErrorMessage('Something went wrong. Please try again later.');
     });
 }
+
